90c63e54f8b899afb74a9c64ad06ecb7f5f41572
lucaszxx5 <lucasd476@gmail.com>
2024-02-21 13:56:04 -0300

    Criação do Header e Página Home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
-
+import { HeaderComponent } from '../../shared/header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [
+    ButtonModule,
+    HeaderComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
